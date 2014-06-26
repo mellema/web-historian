233e9504010cc11bcb63ec3ebe84a86288a83a99
Walker Mellema <walkermellema@gmail.com>
2014-06-25 21:28:39 -0700

    basic write to txt file is working

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,44 @@
 var path = require('path');
+var http = require('http'),
+    fs = require('fs');
+
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var method = req.method;
+  if(method === 'GET'){
+    fs.readFile('./public/index.html', function (err, html) {
+      if (err) {
+        throw err; 
+      }
+      res.write(html);  
+      res.end(archive.paths.list);
+    });
+  }
+  if(method === 'POST'){
+    console.log('************************** ');
+    console.log("[200] " + req.method + " to " + req.url);
+    var urlInput = '';
+    req.on('data', function(chunk) {
+      console.log("Received body data:");
+      urlInput = chunk.toString();
+      urlInput = urlInput.slice(4);
+      console.log(urlInput);
+    });
+    
+    req.on('end', function() {
+      // empty 200 OK response for now
+      fs.appendFile('../archives/sites.txt', urlInput, function (err) {
+        console.log(err);
+      });
+      res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+      res.end();
+    });
+
+  }
 };
+var http = require('http'),
+    fs = require('fs');
+
+
