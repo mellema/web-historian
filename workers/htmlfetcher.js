be88aa52c8f846fc1e3ccd8939045d1c323bc315
Walker Mellema <walkermellema@gmail.com>
2014-06-26 11:26:11 -0700

    Figured out the hard part, just need some logic

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,39 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
+var http = require('http'),
+    fs = require('fs');
+var request = require('request');
+
+fs.readFile('../archives/sites.txt', function (err, html) {
+  if (err) {
+    throw err; 
+  }
+  var str = html.toString();
+  var urlArray = str.split('\n');
+  var found = false;
+  var filesArray = fs.readdirSync('../archives/sites');
+  for(var i = 0; i < urlArray.length; i++){
+    var site = 'http://' + urlArray[i];
+    for(var j = 0; j < filesArray.length; j++){
+      if(filesArray[j] === urlArray[i]){
+        found = true;
+        console.log(found);
+        break;
+      }
+    }
+    if(!found){
+      console.log(found);
+    }
+  }
+
+  // request('http://www.google.com', function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     //console.log(body) // Print the google web page.
+  //   }
+  // });
+  http.createServer(function(request, response) {  
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    //response.write(html);  
+    response.end();  
+  }).listen(8000);
+});
